288067a19debbd5d598c9b6e3b5c2a34712dfabc
Tomás Freres <uncovered@me.com>
2019-08-13 09:33:33 +0200

    Created JFrame and Gamefield Class

---
+++ src/de/freres/Gamefield.java
@@ -0,0 +1,7 @@
+package de.freres;
+
+import javax.swing.*;
+
+public class Gamefield extends JPanel {
+
+}
--- src/de/freres/Main.java
+++ src/de/freres/Main.java
@@ -1,8 +1,18 @@
 package de.freres;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("test");
+        JFrame field = new JFrame("FESInvaders");
+
+        field.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        field.setContentPane(new Gamefield());
+
+        field.pack();
+
+        field.setVisible(true);
     }
 }
