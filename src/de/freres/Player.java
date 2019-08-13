3bb3645a46421a00141d3090a76dd9c22818bafd
shorvat17 <shorvat@dxc.com>
2019-08-13 14:15:08 +0200

    structure for movement of player

--- src/de/freres/Player.java
+++ src/de/freres/Player.java
@@ -1,11 +1,13 @@
 package de.freres;
 
+
+
 public class Player extends Token {
     private String name;
     private int score;
 
 
-    public Player(String name){
+    public Player(String name) {
         this.name = name;
         livePoints = 3;
     }
