1a84a717d0cd280b7f57252ff18cd41389259512
Brolus <ole.pahlkoetter@t-online.de>
2019-08-13 14:18:38 +0200

    Delete Vektor and start with class Shoot

--- src/de/freres/Shoot.java
+++ src/de/freres/Shoot.java
@@ -1,4 +1,15 @@
 package de.freres;
 
 public class Shoot {
+
+    private Token pos;
+    private Token vel;
+
+    public Shoot(){
+        pos = new Token(); //Position des Raumschiffs
+        vel = new Token(); //Position des Schusses
+    }
+
+
+
 }
--- src/de/freres/Vektor.java
+++
@@ -1,4 +0,0 @@
-package de.freres;
-
-public class Vektor {
-}
