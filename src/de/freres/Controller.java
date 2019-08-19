97bb2e5a1c72e8b9511f2198665617b0274c9eee
Tomás Freres <uncovered@me.com>
2019-08-19 14:53:25 +0200

    ~

--- src/de/freres/Controller.java
+++ src/de/freres/Controller.java
@@ -11,13 +11,14 @@ public class Controller extends Canvas {
     private ArrayList<Alien> aliens;
     private ArrayList<Shot> shots;
     private Player player;
+    JFrame field;
 
     public Controller() throws InterruptedException {
         this.aliens = new ArrayList<>();
         this.shots = new ArrayList<>();
         //this.player = new Player();
 
-        JFrame field = new JFrame("FESInvaders");
+        field = new JFrame("FESInvaders");
 
         field.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -34,6 +35,8 @@ public class Controller extends Canvas {
     public void refreshDisplay(Graphics g) throws InterruptedException {
         for(int i = 0; i <200; i++){
             TimeUnit.MILLISECONDS.sleep(100);
+            field.repaint();
+            field.revalidate();
 
             for (Alien k: aliens) {
                 k.draw(g);
@@ -49,6 +52,14 @@ public class Controller extends Canvas {
 
 
     public void paint(Graphics g){
+
+
+        try {
+            TimeUnit.MILLISECONDS.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        setBackground(Color.black);
         Alien a = new Alien(30, 50);
         Alien b = new Alien(50, 50);
         Alien c = new Alien(70, 50);
@@ -58,9 +69,11 @@ public class Controller extends Canvas {
         Alien ge = new Alien(50, 70);
 
 
-        Shot sa = new Shot(140,50, "up");
+        for(Shot k: shots){
+            k.draw(g);
+        }
 
-        setBackground(Color.BLACK);
+
         a.draw(g);
         b.draw(g);
         c.draw(g);
@@ -68,20 +81,20 @@ public class Controller extends Canvas {
         e.draw(g);
         f.draw(g);
         ge.draw(g);
-        sa.draw(g);
+        //sa.draw(g);
 
         g.drawString("Highscore: ", 130, 50);
 
         c.destroy();
 
-        Player player = new Player("Gott", 200, 450);
-        player.draw(g);
+        //Player player = new Player("Gott", 200, 450);
+        //player.draw(g);
 
-        try {
+     /*   try {
             this.refreshDisplay(g);
         } catch (InterruptedException ex) {
             ex.printStackTrace();
-        }
+        }*/
     }
 
 
