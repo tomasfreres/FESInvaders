a8afbc22bd45bd18b2ef5be47385af4d32ceee3d
Brolus <ole.pahlkoetter@t-online.de>
2019-08-26 13:43:04 +0200

    Update

--- src/de/freres/KeyEventClass.java
+++ src/de/freres/KeyEventClass.java
@@ -17,20 +17,16 @@ public class KeyEventClass implements KeyListener {
         down = keys[KeyEvent.VK_S];
         left = keys[KeyEvent.VK_A];
         right = keys[KeyEvent.VK_D];
-        //System.out.println("" + left + "" + right + "" + up + "" + down);
         player.move(left,right, up, down);
 
     }
     @Override
     public void keyPressed(KeyEvent e) {
         keys[e.getKeyCode()] = true;
-        System.out.println("keyPressed " + e.getKeyCode() + " | " + KeyEvent.VK_W + " is " + keys[KeyEvent.VK_W]);
     }
     @Override
     public void keyReleased(KeyEvent e) {
-        if (up = true){
-            keys[e.getKeyCode()] = true;
-        }
+            keys[e.getKeyCode()] = false ;
     }
     @Override
     public void keyTyped(KeyEvent e) {
